394c81cb343dfe73277f0abaffd677b4627c9d30
Francisco Arenas <wook82@gmail.com>
2022-01-19 18:02:50 -0300

    array y slice

--- src/main.go
+++ src/main.go
@@ -3,20 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("hola") // defer se ejecuta al final, por eso impime al final "hola"
-	fmt.Println("mundo")
+	// array, el largo es inmutable
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
 
-	for i := 0; i < 10; i++ {
-		fmt.Println(i)
+	fmt.Println(array)
+	fmt.Println("largo del array ", len(array))
+	fmt.Println("capacidad de la array ", cap(array))
 
-		if i == 2 {
-			fmt.Println("es 2")
-			continue
-		}
+	// slice, son mutables
+	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
 
-		if i == 8 {
-			fmt.Println("break")
-			break
-		}
-	}
+	fmt.Println(slice)
+	fmt.Println("largo del slice ", len(slice))
+	fmt.Println("capacidad del slice ", cap(slice))
+
+	// metodos en el slice
+	fmt.Println(slice[0])   //imprime el elemento de la posicion 0
+	fmt.Println(slice[:3])  //imprime hasta la posicion 3, posicion 3 es exclusivo
+	fmt.Println(slice[5:8]) //imprime entre la posicion 5 y 8, aca las posiciones son inclusivas
+	fmt.Println(slice[4:])  //imprime desde el elemento 4 en adelante, posicion exclusivo
+
+	// agregar elementos al slice
+	slice = append(slice, 10)
+	fmt.Println(slice)
+
+	newSlice := []int{11, 12, 13, 14, 15, 16, 17, 18, 19, 20}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
