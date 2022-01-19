dac50bef09add54ba31d47fd5cc23c6c96a2f361
Francisco Arenas <wook82@gmail.com>
2022-01-18 22:08:07 -0300

    keywords defer, break y continue

--- src/main.go
+++ src/main.go
@@ -3,31 +3,20 @@ package main
 import "fmt"
 
 func main() {
+	defer fmt.Println("hola") // defer se ejecuta al final, por eso impime al final "hola"
+	fmt.Println("mundo")
 
-	isOdd(2)
-	isOdd(5)
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
 
-	fmt.Println()
+		if i == 2 {
+			fmt.Println("es 2")
+			continue
+		}
 
-	// sin condicion, para multiples variables
-	value := 200
-	switch {
-	case value > 100:
-		fmt.Println("Es mayor a 100")
-	case value < 0:
-		fmt.Println("Es menor a 0")
-	default:
-		fmt.Println("No condicion")
-	}
-}
-
-func isOdd(number int) {
-	//asi o del otro modo, anidado ene l switch
-	//modulo := number % 2
-	switch modulo := number % 2; modulo {
-	case 0:
-		fmt.Println("false, numero ", number, " es par")
-	default:
-		fmt.Println("true, numero ", number, " es impar")
+		if i == 8 {
+			fmt.Println("break")
+			break
+		}
 	}
 }
