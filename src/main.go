c9f32f43bf27160fc225c4fcd34d6db19b97fe00
Francisco Arenas <farenas@NI-NTB-120.local>
2022-01-21 11:27:16 -0300

    struct

--- src/main.go
+++ src/main.go
@@ -1,33 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+type car struct {
+	brand string
+	year  int
+	color string
+}
 
 func main() {
-	// es mas eficiente que usar slice, usa concurrencia por lo que
-	// los valores puden no recorrerse en orden
-	m := make(map[string]int)
-
-	m["Jose"] = 14
-	m["Pepito"] = 20
-
-	fmt.Println(m)
-
-	// un valor
-	fmt.Println(m["Jose"])
-	fmt.Println(m["Pepito"])
-	fmt.Println(m["no existe"]) // es cero
-
-	//verificar si excte en el diccionario
-	value, ok := m["Pepito"]
-	fmt.Println(value, ok) // OK = true
-
-	valueNotFount, nok := m["not found"]
-	fmt.Println(valueNotFount, nok) // NOK = false
-
-	// recorrer el map
-	for i, valor := range m {
-		fmt.Println(i, valor)
+	myCar := car{
+		brand: "Ford",
+		year:  2022,
 	}
+
+	fmt.Println(myCar)
+
+	var otherCar car
+	otherCar.brand = "Ferrari"
+
+	fmt.Println(otherCar)
 }
