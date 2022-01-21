d3857c0d66960650c18e96b422d2ec15581bade0
Francisco Arenas <farenas@NI-NTB-120.local>
2022-01-21 14:22:02 -0300

    structs y punteros y funciones de structs

--- src/main.go
+++ src/main.go
@@ -2,20 +2,49 @@ package main
 
 import (
 	"fmt"
-
-	pk "github.com/w00k/curso_golang_platzi/src/mypackage"
 )
 
+// pc es un struct para el ejemplo
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
+
+// ping método de struc 'pc', que imprime la marca
+func (myPc pc) ping() {
+	fmt.Println(myPc.brand, " Pong")
+}
+
+// duplicateRAM es una función que permite acceder al atributo 'ram' y multiplicarlo por 2
+// usando punteros, mejor practica
+func (myPc *pc) duclicateRAM() {
+	myPc.ram = myPc.ram * 2
+}
+
 func main() {
-	myCar := pk.CarPublic{
-		Brand: "Ford",
-		Year:  2022,
+	a := 50
+	b := &a
+
+	fmt.Println(a)  // valor
+	fmt.Println(b)  // dirección de memoria
+	fmt.Println(*b) // valor de la dirección de memoria
+
+	*b = 100 // nuevo valor a la dirección de memoria
+
+	fmt.Println(a) // valor, a y *b están apuntando a la misma dirección de memoria
+
+	myPc := pc{
+		ram:   16,
+		disk:  200,
+		brand: "msi",
 	}
 
-	fmt.Println(myCar)
+	fmt.Println(myPc)
+	myPc.ping()
 
-	var otherCar pk.CarPublic
-	otherCar.Brand = "Ferrari"
+	//myPc.ram = 32 // puede ser rebundante
 
-	fmt.Println(otherCar)
+	myPc.duclicateRAM() // convención para modificar valores de atributos
+	fmt.Println(myPc)
 }
