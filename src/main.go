8e2db5b83c112422d34dab1360514904261fc70f
Francisco Arenas <farenas@NI-NTB-120.local>
2022-01-24 11:47:06 -0300

    goroutines y funciones anónimas

--- src/main.go
+++ src/main.go
@@ -1,46 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-// interfaz que ejecuta a la función area
-type figura2D interface {
-	area() float64
-}
-
-// cuadrado struct que contiene el lado del cuadrado
-type cuadrado struct {
-	base float64
-}
-
-// area implementación de la función de la interface
-func (c cuadrado) area() float64 {
-	return c.base * c.base
-}
-
-// rectangulo struct que contiene los lados del rectangulo
-type rectangulo struct {
-	base   float64
-	altura float64
-}
-
-// area implementación de la función de la interface
-func (r rectangulo) area() float64 {
-	return r.base * r.altura
-}
-
-func calcular(f figura2D) {
-	fmt.Println("area:", f.area())
+func say(text string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(text)
 }
 
 func main() {
-	myCuadrado := cuadrado{base: 2}
-	myRectangulo := rectangulo{base: 2, altura: 4}
+	var wg sync.WaitGroup
 
-	// con interfaz
-	calcular(myCuadrado)
-	calcular(myRectangulo)
+	fmt.Println("Hello")
 
-	// lista de interfaces
-	myInterface := []interface{}{"Hola", 12, 4.90}
-	fmt.Println(myInterface...)
+	wg.Add(1)
+	// concurrencia con goroutines
+	go say("world", &wg)
+	wg.Wait()
+
+	// funciones anónimas
+	go func(text string) {
+		fmt.Println(text)
+	}("adios")
+	// agrego tiempo de espera para que se imprime la goroutines
+	time.Sleep(time.Second * 1)
 }
